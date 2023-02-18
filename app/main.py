22270548eca0944eb383280e453ad262f58b4972
Fauzaan <fauzaan.mm@gmail.com>
2023-02-18 15:47:34 +0800

    v2.4
    Needs some more custom responses.
    Documentation required.
    Everything else is done

--- app/main.py
+++ app/main.py
@@ -1,53 +1,10 @@
-from fastapi import FastAPI, Response, Request, File, UploadFile
+from fastapi import FastAPI, Response, Request, File, HTTPException
 from typing import Optional 
-import redis
 from redis_functions import RedisInterface
 import ticket
 import uuid
 
-"""
-Sample request body:
-{ 
-    "ticket_id":"f82b4dkc",
-    "name": "Fauzaan",
-    "from_city" : "Singapore",
-    "to_city" : "Berkley",
-    "gate" : 48,
-    "price" : 876.12,
-    "date": "5th June 2024"
-}
-Sample request body to insert many:
-[
-    { 
-        "ticket_id":"f82b4dkc",
-        "name": "Ted",
-        "from_city" : "Prague",
-        "to_city" : "Washington",
-        "gate" : 12,
-        "price" : 1000,
-        "date": "5th June 2024"
-    },
-    { 
-        "ticket_id":"f82b4dkc",
-        "name": "Lily",
-        "from_city" : "England",
-        "to_city" : "Washington",
-        "gate" : 48,
-        "price" : 1020,
-        "date": "10th June 2024"
-    },
-    { 
-        "ticket_id":"f82b4dkc",
-        "name": "Boldimir",
-        "from_city" : "Iceland",
-        "to_city" : "Helsinki",
-        "gate" : 11,
-        "price" : 80,
-        "date": "9th June 2024"
-    }
-]
 
-"""
 
 app = FastAPI()
 redisThings = RedisInterface()
@@ -56,9 +13,9 @@ redisThings = RedisInterface()
 Description:
     Root url. To indicate that all is well.
 Parameters:
-
+    None
 Returns:
-
+    String
 """
 @app.get("/")
 def read_root(request: Request):
@@ -70,24 +27,21 @@ def read_root(request: Request):
 
 """
 Description:
-    This method is created seperately because, we can use the pipeline function to batch insert
-    multiple values, instead of calling the redisThings.insert function multiple times. 
+    Helps create ticket with along with ticket id
 Parameters:
 
 Returns:
 
 """
 @app.post("/create_ticket")
-async def create_ticket(ticket: ticket.Ticket, response: Response):
-    # print("ticket", ticket)
-    if not ticket:
-        response.status_code = 404
-        return "Entity was not found"
+async def create_ticket(ticket: ticket.Ticket):
+    if not ticket.date or not ticket.name or not ticket.from_city or not ticket.price or not ticket.to_city or not ticket.gate:
+        raise HTTPException(status_code=400, detail="Bro something is wrong (hint: there's null values somewhere in your request). Fix it") 
     ticket.ticket_id = str(uuid.uuid4())
     redisThings.insert(ticket)
-    """
-    Code to check if it has indeed been created
-    """
+
+    if not redisThings.get_by_key(ticket.ticket_id):
+        return "Failed to create for some reasons"
 
     return ticket
 
@@ -101,19 +55,22 @@ Description:
     multiple values, instead of calling the redisThings.insert function multiple times. 
 Parameters:
 
-Returns:
 
+Returns:
+    Custom HTTP response if number of fields are incorrect
 """
 @app.post("/create_many_tickets")
 async def create_many_tickets(ticketList: list, response: Response):
+
     fields = {
-    "ticket_id",
-    "name",
-    "from_city",
-    "to_city",
-    "gate",
-    "price",
-    "date"
+        "ticket_id",
+        "name",
+        "from_city",
+        "to_city",
+        "gate",
+        "price",
+        "date",
+        "file"
     }
     """
         Finding union is much faster below, as otherwise, we have to perform nested loops with
@@ -122,9 +79,8 @@ async def create_many_tickets(ticketList: list, response: Response):
     for ticket in ticketList:
         union = fields.union(ticket.keys())
         # this is a check to see if the fields are consistent
-        if len(union) > 7:
-            response.status_code = 404
-            return "Incorrect number of fields or field names!"
+        if len(union) > 8:
+            raise HTTPException(status_code=404, detail="Incorrect number of fields bRo")
         ticket['ticket_id'] = str(uuid.uuid4())
     redisThings.insert_many(ticketList)
     return ticketList
@@ -133,26 +89,51 @@ async def create_many_tickets(ticketList: list, response: Response):
 
 
 
+
 """
 Description:
-    This method uploads a file to the key mentioned in  
+    This method uploads a file to the key mentioned in the path. This is seperated from the 
+    insert function because insert() inserts a ticket object, that is converted to a dict 
+    before uploading
+
+    upload file receives a dict object and directly uploads it by appending the file object 
+    to it as a string
+
 Parameters:
+    ticket_id, from the path
+    fileName, which is a bytes object from the body of the request
 
 Returns:
 
 """
-@app.post("/upload_file/{ticket_id}")
-def upload_file(ticket_id: str, fileName: bytes = File()):
-    data = redisThings.get_by_key(ticket_id)
-    if data:
-        # file_to_upload = fileName.read()
-        data['file'] =  fileName
-        redisThings.insert(data)
-        return data
+@app.put("/upload_file/{ticket_id}")
+async def upload_file(ticket_id: str, fileName: bytes = File()):
+    keys = redisThings.r.keys()
+    if ticket_id not in keys:
+        raise HTTPException(status_code=400, detail="Key does not exist")
+    ticket = redisThings.get_by_key(ticket_id)
+
+    if ticket:
+        ticket = {
+            "ticket_id": ticket["ticket_id"],
+            "name": ticket["name"],
+            "from_city" : ticket["from_city"],
+            "to_city" : ticket["to_city"],
+            "gate" : ticket["gate"],
+            "price" : ticket["price"],
+            "date": ticket["date"],
+            "file": fileName.decode('ascii')
+        }
+        redisThings.upload_file(ticket)
+        if redisThings.get_by_key(ticket["ticket_id"]): 
+            return "created successfully"
+        else:
+            raise HTTPException(status_code=400, detail="Upload file error")
+    else:
+        raise HTTPException(status_code=400, detail="ID not found")
 
 
 
-# there is change
 
 
 """
@@ -166,9 +147,6 @@ Returns:
 """
 @app.delete("/delete_ticket_by_id/{ticket_id}")
 def delete_ticket_by_id(ticket_id: str):
-    """
-    Code to delete a ticket by ticket id from redis db
-    """
     redisThings.delete_by_key(ticket_id)
     return "Key was deleted successfully"
 
@@ -187,10 +165,9 @@ Returns:
 """
 @app.delete("/delete_all")
 def delete_all_tickets():
-    """
-    Code to delete a ticket by ticket id from redis db
-    """
     redisThings.delete_all()
+    if redisThings.get_all():
+        return "Keys were not deleted successfully"
     return "Keys were all deleted successfully"
 
 
@@ -200,8 +177,7 @@ def delete_all_tickets():
 
 """
 Description:
-    This method is created seperately because, we can use the pipeline function to batch insert
-    multiple values, instead of calling the redisThings.insert function multiple times. 
+    This method helps retrieve all the tickets in the  redis database.
 Parameters:
 
 Returns:
@@ -215,23 +191,22 @@ def get_all_tickets(
     limit: Optional[int] = None,
     offset: Optional[int] = None
     ):
-    try:
-        data = redisThings.get_all()
-        if sortBy:
-            data = sorted(data, key=lambda d: d[sortBy])
+  
+    data = redisThings.get_all()
+    if sortBy:
+        data = sorted(data, key=lambda d: d[sortBy])
 
-        if limit:
-            if offset:
-                data = data[0 if (offset<0 or offset>len(data)) else offset :]
-            # print(data)
-            data = data[: len(data) if (limit > len(data) or limit < 0) else limit]
-
-            return data if data else "data is empty"
-
+    if limit:
         if offset:
-            data = data[0 if offset<0 else offset :]
-    except: 
-        print("There was an error")
+            data = data[0 if (offset<0 or offset>len(data)) else offset :]
+        data = data[: len(data) if (limit > len(data) or limit < 0) else limit]
+
+        return data if data else "data is empty"
+
+    if offset:
+        data = data[0 if offset<0 else offset :]
+   
+        
     return data if data else "data is empty"
     
 
@@ -241,16 +216,23 @@ def get_all_tickets(
 
 """
 Description:
-    This method assumes you know the ticket id somehow 
+    This method assumes you know the ticket id, and retreives the ticket based on the id 
 Parameters:
+    From
 
 Returns:
 
 """
 @app.get("/get_ticket_by_id/{ticket_id}")
-def get_ticket_by_id(ticket_id:str):
+def get_ticket_by_id(ticket_id: Optional[str] = None):
+    if not ticket_id:
+        data = redisThings.get_all()
+
     data = redisThings.get_by_key(ticket_id)
-    return data if data else "data is empty"
+    if data:
+        return data
+    else:
+        raise HTTPException(status_code=200, detail="Data is empty bRo")
 
 
 
--- app/redis_functions.py
+++ app/redis_functions.py
@@ -19,28 +19,28 @@ class RedisInterface:
             print("\n\n\nRedis service is running")
         else:
             print("\n\n\nRedis server error")
+
+    # good
     def insert(self, ticket: ticket.Ticket):
         if self.r:
-                val = { 
-                    "ticket_id": ticket.ticket_id,
-                    "name": ticket.name,
-                    "from_city" : ticket.from_city,
-                    "to_city" : ticket.to_city,
-                    "gate" : ticket.gate,
-                    "price" : ticket.price,
-                    "date": ticket.date
-
-                }
-                self.r.set(ticket.ticket_id, json.dumps(val))
+            val = {
+                "ticket_id": ticket.ticket_id,
+                "name": ticket.name,
+                "from_city" : ticket.from_city,
+                "to_city" : ticket.to_city,
+                "gate" : ticket.gate,
+                "price" : ticket.price,
+                "date": ticket.date,
+                "file": ticket.file.decode('ascii') if ticket.file else ""
+            }
+            self.r.set(ticket.ticket_id, json.dumps(val))
         else:
-                print("\n\n\nUnable to insert the values!")    
+            print("\n\n\nUnable to insert the values! Redis instance was not instantiated properly")    
     
     def insert_many(self, ticketList: list):
         if self.r:
-            print(ticketList)
             pipe = self.r.pipeline() # this is the main difference between insert and insert many
             for ticket in ticketList:
-                print("ticket", ticket)
                 val = { 
                     "ticket_id": ticket["ticket_id"],
                     "name": ticket["name"],
@@ -48,12 +48,20 @@ class RedisInterface:
                     "to_city" : ticket["to_city"],
                     "gate" : ticket["gate"],
                     "price" : ticket["price"],
-                    "date": ticket["date"]
+                    "date": ticket["date"],
+                    "file": ticket["file"].decode('ascii') if "file" in ticket else " "
                 }
                 self.r.set(ticket['ticket_id'], json.dumps(val))
             pipe.execute()
         else:
             print("\n\n\nUnable to insert the values, as there was a redis error!")        
+    
+    def upload_file(self, ticket: dict):
+        if self.r:
+            self.r.set(ticket["ticket_id"], json.dumps(ticket))
+        else:
+            print("\n\n\nUnable to insert the values! Redis instance was not instantiated properly")    
+    
 
     def get_all(self):
         if self.r:
@@ -63,15 +71,13 @@ class RedisInterface:
                 data.append(json.loads(self.r.get(key)))
             return data
 
+    #good
     def get_by_key(self, key: str):
         if self.r:
-            return json.loads(self.r.get(key))
+            data = self.r.get(key)
+            return json.loads(data) if data else None
         else:
-            print("\n\n\nUnable to insert the values, as there was a redis error!")
-
-    
-    def upload_file(self, file: bytes):
-        pass
+            return "\n\n\nUnable to get the values, as there was a redis error!"
 
 
     def delete_all(self):
